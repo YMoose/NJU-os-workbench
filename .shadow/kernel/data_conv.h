fabfdb85388da0adcbf764a8fb7bd6c23e78f653
tracer-nju <tracer@nju.edu.cn>
2023-07-24 14:10:28 +0000

    Linux ubs2004 5.4.0-153-generic #170-Ubuntu SMP Fri Jun 16 13:43:31 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:10:28 up 17:35,  0 users,  load average: 0.16, 0.19, 0.13

--- .shadow/kernel/data_conv.h
+++ .shadow/kernel/data_conv.h
@@ -3,6 +3,6 @@
 
 #define ptr_char(p) (*(char*)(p)) 
 
-#define ptr_le_u32(p) (((unsigned int)ptr_char(p+3))|(((unsigned int)ptr_char(p+2))<<8)|(((unsigned int)ptr_char(p+1))<<16)|(((unsigned int)ptr_char(p))<<24))
+#define ptr_le_u32(p) (((unsigned int)ptr_char(p))|(((unsigned int)ptr_char(p+1))<<8)|(((unsigned int)ptr_char(p+2))<<16)|(((unsigned int)ptr_char(p+3))<<24))
 
 #endif /* data_conv_h */
